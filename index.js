245784a9c65928872b79df56aec7abf3268a967a
RyanYoung306 <ryan.young.2022@mumail.ie>
2023-11-28 23:06:36 +0000

    deleted

--- index.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    setTimeout(() => {
-        document.getElementById('loveText').classList.add('visible');
-    }, 100); // Adjust the time as needed
-});
